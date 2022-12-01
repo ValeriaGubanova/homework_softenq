649263d31c452035decc126b6af04aaffee0bd3e
Gubanova valeria <gubanovavaleria07@gmail.com>
2022-12-01 23:55:05 +0600

    Использованы onmouseenter и onmouseout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 function clickPhoto() {
-    alert("Губанова Валерия")
+    alert("Губанова Валерия");
+}
+function moveOnPhoto(event) {
+    console.log("On the block", event);
+}
+function moveOutPhoto(event) {
+    console.log("Out the block", event);
 }
